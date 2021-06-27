7ed9fb7c7c3a6e6c5421d28ad6a9474231dfea75
Roberto <rorepoid@gmail.com>
2021-06-27 17:57:16 -0500

    :sparkles: Validate health on each attack
    for more pleasure

--- public/index.php
+++ public/index.php
@@ -10,3 +10,23 @@ $soldier2 = new Soldier("Zeus");
 
 // Present game
 printf("%s vs %s \n", $soldier1->getName(), $soldier2->getName());
+
+// Start game
+while (true) {
+    $soldier1->attack($soldier2);
+    if (!$soldier1->isAlive() || !$soldier2->isAlive()) {
+        break;
+    }
+    $soldier2->attack($soldier1);
+}
+
+if ($soldier1->isAlive()) {
+    $killerSoldier = $soldier1;
+    $deadSoldier = $soldier2;
+} else {
+    $killerSoldier = $soldier2;
+    $deadSoldier = $soldier1;
+}
+
+printf("\n%s has been killed by %s \n", $deadSoldier->getName(), $killerSoldier->getName());
+printf("Game is over, %s wins\n", $killerSoldier->getName());
--- src/Soldier.php
+++ src/Soldier.php
@@ -4,14 +4,47 @@ namespace App;
 
 final class Soldier {
 	private $name;
+	private $health;
 
 	public function __construct(string $name)
 	{
 		$this->name = $name;
+		$this->isAlive = true;
+		$this->health = 10;
 	}
 
 	public function getName()
 	{
 		return $this->name;
+	}
+
+	public function attack(Soldier $soldier)
+	{
+		printf("\n%s attacks %s \n", $this->name, $soldier->name);
+		$soldier->recievesAttack();
+	}
+
+	private function recievesAttack()
+	{
+		printf("%s received attack \n", $this->name);
+
+		$this->health -= 5;
+
+		printf("%s health is now %s \n", $this->name, $this->health);
+
+		if ($this->health <= 0) {
+			$this->die();
+		}
+	}
+
+	private function die()
+	{
+		printf("%s died \n", $this->name);
+		$this->isAlive = false;
+	}
+
+	public function isAlive()
+	{
+		return $this->health > 0;
 	}
 }
